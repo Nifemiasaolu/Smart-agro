0dd80889af73afba67e1a8f109dcad498580deef
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-28 11:48:32 +0100

    images constants

---
+++ src/Constants/images.js
@@ -0,0 +1,64 @@
+import article1 from "../assets/article1.png";
+import article2 from "../assets/article2.png";
+import article3 from "../assets/article3.png";
+import articleFaintStyle from "../assets/articleFaintStyle.png";
+import bg1 from "../assets/bg1.png";
+import bg2 from "../assets/bg2.png";
+import blueberry from "../assets/blueberry 2.png";
+import botanic from "../assets/botanic.png";
+import bottomStyle from "../assets/bottomStyle.png";
+import cabbage from "../assets/cabbage.png";
+import carrot from "../assets/carrot.png";
+import eggplant from "../assets/eggplant.png";
+import exploreProjectStyle from "../assets/exploreProjectStyle.png";
+import figma from "../assets/figma.png";
+import quote from "../assets/quote.png";
+import growth from "../assets/growth.png";
+import apple from "../assets/Icon_Apple.png";
+import strawberry from "../assets/icon-Strawberry.png";
+import investment from "../assets/investment.png";
+import leaf from "../assets/leaf.png";
+import organicFarm from "../assets/organicFarm.png";
+import productIcon from "../assets/productIcon.png";
+import productImg from "../assets/productImg.png";
+import productStyle from "../assets/productStyle.png";
+import productVegetable from "../assets/productVegetables.png";
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+import project3 from "../assets/project3.png";
+import project4 from "../assets/project4.png";
+import testimonial from "../assets/testimonial.png";
+
+// eslint-disable-next-line
+export default {
+  article1,
+  article2,
+  article3,
+  articleFaintStyle,
+  bg1,
+  bg2,
+  blueberry,
+  botanic,
+  bottomStyle,
+  cabbage,
+  carrot,
+  eggplant,
+  exploreProjectStyle,
+  figma,
+  quote,
+  growth,
+  apple,
+  strawberry,
+  investment,
+  leaf,
+  organicFarm,
+  productIcon,
+  productImg,
+  productStyle,
+  productVegetable,
+  project1,
+  project2,
+  project3,
+  project4,
+  testimonial,
+};
