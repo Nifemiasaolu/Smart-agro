f0d074d5efca14bfad6b2f1674839c55a2726d74
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-28 12:31:12 +0100

    Component setup

---
+++ src/Pages/index.js
@@ -0,0 +1,22 @@
+import AboutUs from "./AboutUs";
+import ContactUs from "./ContactUs";
+import Display from "./Display";
+import Header from "./Header";
+import Navbar from "../Features/Navbar";
+import News from "./News";
+import Products from "./Products";
+import Projects from "./Projects";
+import Services from "./Services";
+import Testimonial from "./Testimonial";
+
+export {
+  AboutUs,
+  ContactUs,
+  Display,
+  Header,
+  News,
+  Products,
+  Projects,
+  Services,
+  Testimonial,
+};
