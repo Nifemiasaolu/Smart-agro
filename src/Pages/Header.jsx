0bf9a1306ddf9f839fbb6362fc5efbb054a7957a
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-29 13:05:38 +0100

    Continous work on detail

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -3,25 +3,28 @@ import { images } from "../Constants";
 
 function Header() {
   return (
-    <div className="app-bg section-padding h-screen flex items-center justify-start">
+    <div className="app-bg section-padding flex h-screen items-center justify-start">
       <div>
         <div className="flex flex-col items-start">
-        <p className="p-gothic text-sm font-bold ">Original & Natural</p>
-        <img src={images.underline} alt="underline" className="w-[20%]"/>
+          <p className="p-gothic text-sm font-bold ">Original & Natural</p>
+          <img src={images.underline} alt="underline" className="w-[20%]" />
         </div>
 
         <div className="relative">
-          <img src={images.leaf} alt="leaf" className="leaf-img absolute 
-            left-[15rem] top-[-1rem] "/>
-          <h1 className="p-livvic font-bold text-[30px] text-golden">Agriculture Matter</h1> <br />
+          <img
+            src={images.leaf}
+            alt="leaf"
+            className="leaf-img absolute 
+            left-[15rem] top-[-1rem] "
+          />
+          <h1 className="p-livvic text-[30px] font-bold text-golden">
+            Agriculture Matter
+          </h1>
+          <h1 className="p-livvic text-[30px] font-bold">Good Production</h1>
         </div>
-        
-        <h1>
-          <span>Good Production</span>
-        </h1>
-        <p>
+        <p className="p-gothic text-xs  ">
           Dissuade ecstatic and properly saw entirely sir why laughter endeavor.
-          In on my jointure horrible margaret suitable he speedily.
+          In <br /> on my jointure horrible margaret suitable he speedily.
         </p>
 
         <Button>Discover More</Button>
