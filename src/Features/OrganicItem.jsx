a0614c37ee398b3bdb0bbd1a8a994741205486bf
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-02 22:36:56 +0100

    Organic list

---
+++ src/Features/OrganicItem.jsx
@@ -0,0 +1,16 @@
+import { images } from "../Constants"
+
+function OrganicItem({organ}) {
+  return (
+    <li className="flex items-center gap-4 ">
+      <div>
+        <img src={images.check} alt="check" className="w-[15px]"/>
+      </div>
+      <div>
+        <p className="text-xs">{organ.text}</p>
+      </div>
+    </li>
+  )
+}
+
+export default OrganicItem
