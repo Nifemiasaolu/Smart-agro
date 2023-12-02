20bd1aef95557bf078ccea6db9b46b98ab604661
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-02 22:37:29 +0100

    Lil style change

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,6 +1,6 @@
 function Button({children}) {
-  const className = `px-3 text-xs mt-4 rounded-[4px] bg-golden font-[Livvic] 
-  cursor-pointer border-none outline-none text-black capitalize font-semibold `
+  const className = `px-2 py-2 text-xs mt-4 rounded-[8px] bg-golden font-[Livvic] 
+  cursor-pointer border-none outline-none text-black uppercase font-semibold `
 
 
   return (
