6238f396965d0ba4a29913c4d91c722cf64a6665
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-06 21:33:50 +0100

    Product component

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,13 +1,20 @@
+import { data } from "../Constants"
+import VegetableItem from "../Features/VegetableItem"
+
 function Products() {
   return (
-    <div>
+    <div id="product" className="section-padding h-screen flex-center flex-col ">
       <div>
         <p>Popular foods and vegetables</p>
         <h1>Quality fruits & vegetables</h1>
       </div>
+        
+      <ul className="flex-center  flex-wrap gap-8 ">
+        {data.vegetables.map(veg => <VegetableItem veg={veg} key={veg.id}/>)}
+      </ul>
     </div>
   )
 }
 
 export default Products
-///
+////
