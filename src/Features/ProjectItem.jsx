2d7848bdec9a0dff610a18d12b18e50e6cbb5fd9
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-10 15:02:59 +0100

    Project list

--- src/Features/ProjectItem.jsx
+++ src/Features/ProjectItem.jsx
@@ -1,7 +1,14 @@
 function ProjectItem({ project }) {
   return (
-    <li className="hover:translate-y-4 duration-200 transition">
-      <img src={project.imgUrl} alt="project" className="w-[5rem] rounded"/>
+    <li className=" flex-1 hover:-translate-y-2 hover:transition
+     hover:duration-200 hover:ease-in-out">
+      <div className="">
+        <img
+          src={project.imgUrl}
+          alt="project"
+          className="aspect-square rounded"
+        />
+      </div>
     </li>
   );
 }
