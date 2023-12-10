a2f04136d83d8973d475f21fefb385977ef725f1
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-10 13:20:47 +0100

    Display details

--- src/Pages/Display.jsx
+++ src/Pages/Display.jsx
@@ -1,14 +1,22 @@
+import { FaPlay } from "react-icons/fa";
+
 function Display() {
   return (
-    <div className="app-bg2 section-padding">
+    <div className="app-bg2 section-padding flex h-[15rem] items-center justify-around">
       <div>
-        <h1 className="">
-          Agriculture Matters to the Future of Bangladesh
+        <h1
+          className="p-livvic text-lg font-bold"
+        >
+          Agriculture Matters to <br /> the Future of Bangladesh
         </h1>
       </div>
+      <div className="border p-2 bg-white rounded-[50%] outline
+       outline-offset-[10px] outline-[0.5px] outline-lightBack">
+        <FaPlay className="text-[10px] text-black"/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Display
+export default Display;
 ////
