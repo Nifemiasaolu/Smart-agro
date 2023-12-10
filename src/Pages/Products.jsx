6f867e08e547bd454fa3c42d7b334d4b19cd2f2b
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-10 15:03:32 +0100

    Product details

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -5,14 +5,18 @@ function Products() {
   return (
     <div
       id="product"
-      className="section-padding flex-center  flex-col mt-[2rem]"
+      className="section-padding flex-center my-[2rem] flex-col"
     >
       <div>
-        <p className="text-xs uppercase p-gothic mb-[1rem]">Popular foods and vegetables</p>
-        <h1 className="capitalize font-bold  p-livvic mb-[1rem] text-center text-lg  ">Quality fruits & vegetables</h1>
+        <p className="p-gothic mb-[10px] text-center text-[10px] uppercase">
+          Popular foods and vegetables
+        </p>
+        <h1 className="p-livvic mb-[1rem]  text-center text-lg font-bold capitalize  ">
+          Quality fruits & vegetables
+        </h1>
       </div>
 
-      <ul className="flex-center  flex-wrap gap-8 ">
+      <ul className="flex-center flex-wrap gap-8 ">
         {data.vegetables.map((veg) => (
           <VegetableItem veg={veg} key={veg.id} />
         ))}
