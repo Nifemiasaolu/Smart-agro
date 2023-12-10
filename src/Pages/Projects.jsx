4786ba1709957290ec39d457ba7a37c94415973f
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-10 15:03:40 +0100

    Projects details

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,11 +1,11 @@
-import { data } from "../Constants";
+import { data, images } from "../Constants";
 import ProjectItem from "../Features/ProjectItem";
 
 function Projects() {
   return (
-    <div className="section-padding pt-8 flex-center flex-col">
+    <div id="project" className="section-padding py-[4rem] bg-darkGreen flex-center relative flex-col">
       <div>
-        <p className="p-gothic mb-[1rem] text-xs uppercase">
+        <p className="p-gothic text-center mb-[10px] text-[10px] uppercase">
           Recently Completed Work
         </p>
         <h1 className="p-livvic mb-[1rem] text-center text-lg font-bold capitalize  ">
@@ -13,9 +13,13 @@ function Projects() {
         </h1>
       </div>
 
-      <ul className="flex flex-wrap gap-4">
+      <ul className=" flex-wrap gap-4 flex-center">
         {data.projects.map(project=> <ProjectItem project={project} key={project.id}/>)}
       </ul>
+
+      <div className="absolute bottom-0">
+        <img src={images.exploreProjectStyle} alt="Project style" />
+      </div>
     </div>
   );
 }
