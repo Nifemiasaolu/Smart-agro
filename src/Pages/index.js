85fc6e332b59c331ef81c8a7e1cab9f7b7d8b979
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-10 15:03:24 +0100

    Tweak on detail

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,7 +2,6 @@ import AboutUs from "./AboutUs";
 import ContactUs from "./ContactUs";
 import Display from "./Display";
 import Header from "./Header";
-import Navbar from "../Features/Navbar/Navbar";
 import News from "./News";
 import Products from "./Products";
 import Projects from "./Projects";
