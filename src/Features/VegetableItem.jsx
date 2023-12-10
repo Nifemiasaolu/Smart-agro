8f57e2ebe0fb665fe95b88e7777dae3445f64c01
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-10 15:03:14 +0100

    Vegetable list

--- src/Features/VegetableItem.jsx
+++ src/Features/VegetableItem.jsx
@@ -3,13 +3,13 @@ function VegetableItem({ veg }) {
     <li
       className="flex-center bg-lightGreen flex-1 flex-col 
       cursor-pointer transition rounded-[15px] p-4 duration-300
-       hover:bg-golden hover:text-black"
+       hover:bg-golden hover:text-black "
     >
-      <div className="pb-4">
+      <div className="pb-4 ">
         <img
           src={veg.imageUrl}
           alt="veg"
-          className="aspect-square w-[1.5rem] "
+          className="aspect-square  "
         />
       </div>
       <div className="text-xs  text-white">{veg.name}</div>
