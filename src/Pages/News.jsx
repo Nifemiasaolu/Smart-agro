6a30c6946c9763f8279f6784c6ebd4cdd21c7cfd
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-11 15:48:36 +0100

    News article

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -1,7 +1,25 @@
+import { images, data } from "../Constants"
+import ArticleItem from "../Features/ArticleItem"
+
 function News() {
   return (
-    <div>
-      News
+    <div id="news" className="bg-darkGreen section-padding">
+      {/* <div>
+        <img src={images.articleFaintStyle} alt="Article style" />
+      </div> */}
+
+       <div>
+        <p className="p-gothic mb-[10px] text-center text-[10px] uppercase">
+          From the Blog
+        </p>
+        <h1 className="p-livvic mb-[1rem]  text-center text-lg font-bold capitalize  ">
+          News & Article
+        </h1>
+      </div>
+
+      <ul>
+        {data.articles.map(article=> <ArticleItem article={article} key={article.id}/>)}
+      </ul>
     </div>
   )
 }
